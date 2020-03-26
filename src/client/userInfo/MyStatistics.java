0144b5bbeaf938f7d01302ddbffd8d961101af02
CrazyHer <877955100@qq.com>
2020-03-26 21:04:58 +0800

    用户信息界面基本完成

--- src/client/userInfo/MyStatistics.java
+++ src/client/userInfo/MyStatistics.java
@@ -7,6 +7,6 @@ public class MyStatistics extends JPanel {
 
     MyStatistics() {
         setBackground(Color.CYAN);
-        add(new JLabel("统计信息"));
+        add(new JLabel("统计信息模块待实现"));
     }
 }
