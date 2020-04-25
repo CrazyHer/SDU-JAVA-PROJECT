42b648d1a4a78167f805a5198943f392795f018b
Coderlizixin <1963933009@qq.com>
2020-04-25 23:28:10 +0800

    修改发布端口

--- src/client/itemList/SoldItemInfoFrame.java
+++ src/client/itemList/SoldItemInfoFrame.java
@@ -1,14 +1,18 @@
 package client.itemList;
 
+import com.alibaba.fastjson.JSON;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
 
 public class SoldItemInfoFrame extends JFrame implements ActionListener {
 
     JPanel panel;
-    JButton btModify;
+    JButton btEdit;
     JButton btDelete;
     JButton btAuction;
     ItemInfo itemInfo;
@@ -19,7 +23,7 @@ public class SoldItemInfoFrame extends JFrame implements ActionListener {
         c.setLayout(new BorderLayout());
         setTitle("商品销售信息");
         setSize(500, 400);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
         panel = new JPanel();
@@ -30,10 +34,10 @@ public class SoldItemInfoFrame extends JFrame implements ActionListener {
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
         btAuction = new JButton("拍卖销售");
-        btModify = new JButton("修改信息");
+        btEdit = new JButton("修改信息");
         btDelete = new JButton("删除商品");
         panel.add(btAuction, new GBC(0, 0, 1, 1).setWeight(0.8, 0.6).setAnchor(GridBagConstraints.NORTHEAST));
-        panel.add(btModify, new GBC(0, 1, 1, 1).setWeight(0.8, 0.6).setAnchor(GridBagConstraints.NORTHEAST));
+        panel.add(btEdit, new GBC(0, 1, 1, 1).setWeight(0.8, 0.6).setAnchor(GridBagConstraints.NORTHEAST));
         panel.add(btDelete, new GBC(0, 2, 1, 1).setWeight(0.8, 0.6).setAnchor(GridBagConstraints.NORTHEAST));
         panel.add(new JPanel(), new GBC(0, 2, 1, 1));
         c.add(panel, BorderLayout.SOUTH);
@@ -46,8 +50,37 @@ public class SoldItemInfoFrame extends JFrame implements ActionListener {
         } else if (e.getActionCommand().equals("修改信息")) {
             //弹出拍卖框
         } else if (e.getSource().equals("删除商品")) {
-
+            try {
+                new NET_DeleteItem(itemInfo.getName());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
+    private class NET_DeleteItem {
+        private final String Command = "DELETE ITEM";//请求类型
+        private final String Address = "localhost";
+        private final int PORT = 2333;//服务器端口
+        private Socket socket;
+        private DataInputStream dis;//输入
+        private DataOutputStream dos;//输出
+        private PrintWriter out;
+
+        private String json;
+
+        public NET_DeleteItem(String itemName) throws IOException {
+            this.socket = new Socket(this.Address, this.PORT);
+            dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            dos = new DataOutputStream(new DataOutputStream(socket.getOutputStream()));
+            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            dos.writeUTF(Command);
+            dos.flush();
+
+            json = JSON.toJSONString(itemName);//使用JSON序列化对象传输过去
+            out.println(json);
+
+            this.socket.close();
+        }
+    }
 }
